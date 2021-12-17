3d4f063f5637083192bfd14e19909eac9f38349b
vhoc <vhocar@gmail.com>
2021-12-17 12:56:24 -0700

    working conditions

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,41 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+import { Alert } from 'react-bootstrap'
 
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
-import useApi from './Helpers'
+//import useApi from './Helpers'
+import useValidateToken from './Api'
 
 const TopBar = ( {location} ) => {
 
-    const [username, setUsername] = useState()
-
     const [auth, setAuth] = useState([])
 
-    const { response, loading, error } = useApi({
-        method: 'get',
-        url: '/validateToken',
-        headers: JSON.stringify({ 'Authorization': localStorage.getItem('token') }),
-    })
+    const { response, loading, error } = useValidateToken(localStorage.getItem('token'))
 
     const goTo = useNavigate()
-    /*
-    const validateToken = async () => {
-        const response = await axios.get('https://paxvox.waxy.app/api/validateToken', { headers: {'Authorization': localStorage.getItem('token')} })
-        const name = await response.data.username
-        setUsername(name)
-    }*/
-
+    
     const logout = () => {
         localStorage.clear()
         goTo('/')
     }
 
     useEffect( () => {
+        
         if (response !== null) {
             setAuth(response)
-        }        
-        localStorage.setItem('username', auth)
+            localStorage.setItem('username', response.username)
+        }
     }, [response] )
 
     return (
@@ -47,15 +38,15 @@ const TopBar = ( {location} ) => {
                     <Navbar.Collapse className={`justify-content-end`}>
                         <Navbar.Text>
                         {loading ? (
-                            <p>loading...</p>
+                            <p>cargando...</p>
                         ) : (
                             <div>
                                 {error && (
                                     <div>
-                                        <p>{error.message}</p>
+                                        <Alert variant='danger'>{error.message}</Alert>
                                     </div>
                                 )}
-                                <div>{auth && <p>{auth.id}</p>}</div>
+                                <div>{auth && <div><span>{auth.username}</span><Button variant='link' onClick={() => logout()}>Salir</Button></div>}</div>
                             </div>
                         )}
                         </Navbar.Text>
