6b68c38f3da34d16ce37af9586931892e2a93e0f
vhoc <vhocar@gmail.com>
2021-12-27 16:35:28 -0700

    Added css print-friendly styles to Reports

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -5,7 +5,7 @@ import DatePicker from 'react-datepicker'
 import TopBar from '../../components/TopBar'
 import ReportBarMeseros from '../../components/Report/ReportBarMeseros'
 import ReportPieChart from '../../components/Report/ReportPieChart'
-import './../../App.css'
+import './Reports.css'
 
 const Reports = ( {username} ) => {
 
@@ -25,84 +25,88 @@ const Reports = ( {username} ) => {
         
         <div >
             <TopBar location={'Mariscos El Rey Obregón'} username={ username }/>
-            <h3>{ 'Reportes' }</h3>
-            <div className='d-flex my-5 justify-content-center section-to-print'>
-                <div className='col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 p-1'>
-                    <span>Fecha inicial</span>
-                    <DatePicker
-                        className="col-12"
-                        selected={startDate}
-                        onChange={date => setStartDate(date)}
-                        placeholderText="Fecha de inicio"
-                    />
+            <div className='reports-container'>
+                <h3 className='print-hidden'>{ 'Reportes' }</h3>
+                <div className='d-flex my-5 justify-content-center section-to-print dates-control'>
+                    <div className='col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 p-1'>
+                        <span>Fecha inicial</span>
+                        <DatePicker
+                            className="col-12"
+                            selected={startDate}
+                            onChange={date => setStartDate(date)}
+                            placeholderText="Fecha de inicio"
+                        />
+                    </div>
+
+                    <div className='col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 p-1'>
+                        <span>Fecha final</span>
+                        <DatePicker
+                            className="col-12"
+                            selected={endDate}
+                            onChange={date => setEndDate(date)}
+                            placeholderText="Fecha final"
+                        />
+                    </div>
+                    
                 </div>
 
-                <div className='col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 p-1'>
-                    <span>Fecha final</span>
-                    <DatePicker
-                        className="col-12"
-                        selected={endDate}
-                        onChange={date => setEndDate(date)}
-                        placeholderText="Fecha final"
+                <div className='d-flex flex-wrap justify-content-around section-to-print'>
+
+                    <ReportBarMeseros
+                        className='flex-fill'
+                        startDate={startDate}
+                        endDate={endDate}
                     />
+
+                    <ReportPieChart
+                        className='flex-fill'
+                        title={`Frecuencia de Visita`}
+                        endpoint={`frecuenciaVisita`}
+                        startDate={startDate}
+                        endDate={endDate}
+                        labels={ ['Primera visita', 'Más de 1 vez al Año', 'Más de 1 vez al Mes', 'Más de 1 vez a la Semana']}
+                    />
+
+                    <ReportPieChart
+                        className='flex-fill'
+                        title={`Atención del Mesero`}
+                        endpoint={`atencionMesero`}
+                        startDate={startDate}
+                        endDate={endDate}
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }                               
+                    />
+
+                    <ReportPieChart
+                        className='flex-fill'
+                        title={`Rapidez en el Servicio`}
+                        endpoint={`rapidezServicio`}
+                        startDate={startDate}
+                        endDate={endDate}
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
+                    />
+
+                    <ReportPieChart
+                        className='flex-fill'
+                        title={`Sabor y Calidad de la Comida`}
+                        endpoint={`calidadComida`}
+                        startDate={startDate}
+                        endDate={endDate}
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
+                    />
+
+                    <ReportPieChart
+                        className='flex-fill'
+                        title={`Experiencia General`}
+                        endpoint={`experienciaGeneral`}
+                        startDate={startDate}
+                        endDate={endDate}
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
+                    />
+
                 </div>
-                
-            </div>
-
-           <div className='d-flex flex-wrap justify-content-around section-to-print'>
-
-                <ReportBarMeseros
-                    className='flex-fill'
-                    startDate={startDate}
-                    endDate={endDate}
-                />
-
-                <ReportPieChart
-                    className='flex-fill'
-                    title={`Frecuencia de Visita`}
-                    endpoint={`frecuenciaVisita`}
-                    startDate={startDate}
-                    endDate={endDate}
-                    labels={ ['Primera visita', 'Más de 1 vez al Año', 'Más de 1 vez al Mes', 'Más de 1 vez a la Semana']}
-                />
-
-                <ReportPieChart
-                    className='flex-fill'
-                    title={`Atención del Mesero`}
-                    endpoint={`atencionMesero`}
-                    startDate={startDate}
-                    endDate={endDate}
-                    labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }                               
-                />
-
-                <ReportPieChart
-                    className='flex-fill'
-                    title={`Rapidez en el Servicio`}
-                    endpoint={`rapidezServicio`}
-                    startDate={startDate}
-                    endDate={endDate}
-                    labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
-                />
-
-                <ReportPieChart
-                    className='flex-fill'
-                    title={`Sabor y Calidad de la Comida`}
-                    endpoint={`calidadComida`}
-                    startDate={startDate}
-                    endDate={endDate}
-                    labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
-                />
-
-                <ReportPieChart
-                    className='flex-fill'
-                    title={`Experiencia General`}
-                    endpoint={`experienciaGeneral`}
-                    startDate={startDate}
-                    endDate={endDate}
-                    labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
-                />
 
             </div>
+
         </div>
          
 
