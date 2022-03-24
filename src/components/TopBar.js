f917ec9ed5eedb34bebd2ecc2de30030935e616b
vhoc <vhocar@gmail.com>
2022-03-24 09:26:04 -0700

    Changed routes in TopBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -17,7 +17,7 @@ const TopBar = ( ) => {
     
     const logout = () => {
         localStorage.clear()
-        goTo('/')
+        goTo('/admin')
     }
 
     useEffect( () => {
@@ -31,7 +31,7 @@ const TopBar = ( ) => {
     return (
 
             <Navbar className={classes.topbar} sticky='top'>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand href="/admin">
                         <small>{localStorage.getItem('location_name')}</small>
                     </Navbar.Brand>
 
@@ -44,7 +44,7 @@ const TopBar = ( ) => {
                         ) : (
                             <div className={classes.rightPane}>
                                 {error && (
-                                    <Navigate to="/" />
+                                    <Navigate to="/admin" />
                                 )}
                                 <div>{auth && <div><span>{auth.username}</span><Button variant='link' onClick={() => logout()}>Salir</Button></div>}</div>
                             </div>
