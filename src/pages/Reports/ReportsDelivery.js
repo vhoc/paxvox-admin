144dc740153bf06175e67552145e12345673f1fe
vhoc <vhocar@gmail.com>
2022-04-05 11:02:44 -0700

    Added ReportsDelivery component

--- src/pages/Reports/ReportsDelivery.js
+++ src/pages/Reports/ReportsDelivery.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
-import DatePicker from 'react-datepicker'
 import axios from 'axios'
-import Navbar from 'react-bootstrap/Navbar'
 
 import TopBar from '../../components/TopBar'
 import DateSelector from '../../components/Report/DateSelector'
 import ReportBarMeseros from '../../components/Report/ReportBarMeseros'
 import ReportPieChart from '../../components/Report/ReportPieChart'
+import ReportPieChart4 from '../../components/Report/ReportPieChart4'
 import './Reports.css'
 
-const Reports = ( {username, location} ) => {
+const ReportsDelivery = ( {username, location} ) => {
 
     const currentDate = new Date()
     const [startDate, setStartDate] = useState(new Date(currentDate.getFullYear(), currentDate.getMonth(), 1))
@@ -44,28 +43,12 @@ const Reports = ( {username, location} ) => {
         <div className='d-flex flex-column justify-content-start'>
             <TopBar location={locationName} username={ username }/>
 
-            <Navbar className='d-flex justify-content-center align-items-start section-to-print mb-3 text-white pb-2 date-selector' sticky='top'>
-                <div className='col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 p-1'>
-                    <span>Desde:</span>
-                    <DatePicker
-                        className="col-12 text-center form-control p-0"
-                        selected={startDate}
-                        onChange={date => setStartDate(date)}
-                        placeholderText="Desde:"
-                    />
-                </div>
-                <div className='col-6 col-sm-5 col-md-4 col-lg-3 col-xl-2 p-1'>
-                    <span>Hasta:</span>
-                    <DatePicker
-                        className="col-12 text-center form-control p-0"
-                        selected={endDate}
-                        onChange={date => setEndDate(date)}
-                        placeholderText="Hasta:"
-                    />
-                </div>
-            </Navbar>
-
-            <DateSelector />
+            <DateSelector
+                handleChangeStartDate={ periodDates => setStartDate(periodDates) }
+                handleChangeEndDate={ periodDates => setEndDate(periodDates) }
+                startDate={startDate}
+                endDate={endDate}
+            />
 
             <div className='d-flex flex-column justify-content-start reports-container mt-3'>               
 
@@ -77,13 +60,13 @@ const Reports = ( {username, location} ) => {
                         endDate={endDate}
                     />
 
-                    <ReportPieChart
+                    <ReportPieChart4
                         className='flex-fill'
                         title={`Frecuencia de Visita`}
                         endpoint={`frecuenciaVisita`}
                         startDate={startDate}
                         endDate={endDate}
-                        labels={ ['Primera visita', 'Más de 1 vez al Año', 'Más de 1 vez al Mes', 'Más de 1 vez a la Semana']}
+                        labels={ [ 'Más de 1 vez al Mes', 'Primera visita', 'Más de 1 vez a la Semana', 'Más de 1 vez al Año'] }
                     />
 
                     <ReportPieChart
@@ -92,7 +75,7 @@ const Reports = ( {username, location} ) => {
                         endpoint={`atencionMesero`}
                         startDate={startDate}
                         endDate={endDate}
-                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }                               
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente',  ] }                           
                     />
 
                     <ReportPieChart
@@ -101,7 +84,7 @@ const Reports = ( {username, location} ) => {
                         endpoint={`rapidezServicio`}
                         startDate={startDate}
                         endDate={endDate}
-                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente',  ] }
                     />
 
                     <ReportPieChart
@@ -110,7 +93,7 @@ const Reports = ( {username, location} ) => {
                         endpoint={`calidadComida`}
                         startDate={startDate}
                         endDate={endDate}
-                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente',  ] }
                     />
 
                     <ReportPieChart
@@ -119,7 +102,7 @@ const Reports = ( {username, location} ) => {
                         endpoint={`experienciaGeneral`}
                         startDate={startDate}
                         endDate={endDate}
-                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente' ] }
+                        labels={ [ 'Muy Mal', 'Mal', 'Regular', 'Bien', 'Excelente',  ] }
                     />
 
                 </div>
@@ -133,4 +116,4 @@ const Reports = ( {username, location} ) => {
 
 }
 
-export default Reports
+export default ReportsDelivery
